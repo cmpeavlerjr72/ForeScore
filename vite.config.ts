845160c78aee158fa6a4c19c908288f0bb2abcf8
Mitchell <cmpeavlerjr@yahoo.com>
2025-06-15 16:09:05 -0400

    Set base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   base:'/ForeScore/',
   build: {
-    outDir: 'build', // Match Render's expected output
+    outDir: 'dist', // Match Render's expected output
   },
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg', '**/*.gif', '**/*.svg'], // Explicitly include image types
 });
